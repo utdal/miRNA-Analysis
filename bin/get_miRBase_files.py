b856e6edc9944f1ecc08051f2f35197930f1c2da
SnehaGummadi <sag200006@utdallas.edu>
2024-10-21 16:24:45 -0500

    update script to get latest references from miRBase

--- bin/get_miRBase_files.py
+++ bin/get_miRBase_files.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+print("We are here.")
+
 from Bio import SeqIO
 import re
 import requests
@@ -12,53 +16,62 @@ import requests
 # hairpin: https://www.mirbase.org/download/hairpin.fa
 # mature: https://www.mirbase.org/download/mature.fa
 
-# TODO How to deal with file paths. As in where is the file path supposed to point?
-#       where is this even downloading?????
+# Link as 10/21/2024
+# miRNA locations: https://www.mirbase.org/download/hsa.gff3
 
 
-class GetMiRBaseFiles:
-    def __init__(self, species_code):
-        self.species = species_code
+def download_miRBase():
+    hairpin_url = 'https://www.mirbase.org/download/hairpin.fa'
+    mature_url = 'https://www.mirbase.org/download/mature.fa'
 
-    def download_miRBase_gff():
-        hairpin_url = 'https://www.mirbase.org/download/hairpin.fa'
-        mature_url = 'https://www.mirbase.org/download/mature.fa'
-
-        response = requests.get(hairpin_url)
-        if response.status_code == 200:
-            with open(f'hairpin.fa', 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f'Error when retrieving hairpin.fa\nError: {response.status_code}')
+    response = requests.get(hairpin_url)
+    if response.status_code == 200:
+        with open(f'hairpin.fa', 'wb') as f:
+            f.write(response.content)
+    else:
+        print(f'Error when retrieving hairpin.fa\nError: {response.status_code}')
         
-        response = requests.get(mature_url)
-        if response.status_code == 200:
-            with open(f'mature.fa', 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f'Error when retrieving mature.fa\nError: {response.status_code}')
+    response = requests.get(mature_url)
+    if response.status_code == 200:
+        with open(f'mature.fa', 'wb') as f:
+            f.write(response.content)
+    else:
+        print(f'Error when retrieving mature.fa\nError: {response.status_code}')
 
 
-    def filter_miRNA_refs(self):
-        # filter for mature human and replace non ACGTU letter with N
+def filter_miRNA_refs():
+    # filter for mature human and replace non ACGTU letter with N
     
-        with open("mature_hsa.fa", "w") as hsa_file, open("mature_other.fa", "w") as other_file:
-            for record in SeqIO.parse("mature.fa", "fasta"):
-                temp = record.name
-                if "hsa" in temp:
-                    sequence = re.sub('[^GATCU]', "N", str(record.seq).upper())
-                    record.seq = sequence
-                    SeqIO.write(record, hsa_file, "fasta")
-                else:
-                    sequence = re.sub('[^GATCU]', "N", str(record.seq).upper())
-                    record.seq = sequence
-                    SeqIO.write(record, other_file, "fasta")
+    with open("mature_hsa.fa", "w") as hsa_file, open("mature_other.fa", "w") as other_file:
+        for record in SeqIO.parse("mature.fa", "fasta"):
+            temp = record.name
+            if "hsa" in temp:
+                #sequence = re.sub('[^GATCU]', "N", str(record.seq).upper())
+                #record.seq = sequence
+                SeqIO.write(record, hsa_file, "fasta")
+            else:
+                #sequence = re.sub('[^GATCU]', "N", str(record.seq).upper())
+                #record.seq = sequence
+                SeqIO.write(record, other_file, "fasta")
         
 
-        with open("hairpin_hsa.fa", "w") as hsa_file:
-            for record in SeqIO.parse("hairpin.fa", "fasta"):
-                temp = record.name
-                if "hsa" in temp:
-                    sequence = re.sub('[^GATCU]', "N", str(record.seq).upper())
-                    record.seq = sequence
-                    SeqIO.write(record, hsa_file, "fasta")
+    with open("hairpin_hsa.fa", "w") as hsa_file:
+        for record in SeqIO.parse("hairpin.fa", "fasta"):
+            temp = record.name
+            if "hsa" in temp:
+                #sequence = re.sub('[^GATCU]', "N", str(record.seq).upper())
+                #record.seq = sequence
+                SeqIO.write(record, hsa_file, "fasta")
+
+def main():
+    print("Generate the miRNA reference files, separating human (hsa) from other organisms.")
+    print("Files will be generated in the directory you are running this script.")
+
+    download_miRBase()
+
+    filter_miRNA_refs()
+
+    print("Completed")
+
+if __name__ == "__main__":
+    main()
