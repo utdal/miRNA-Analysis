64a90fe623b9ab7cbd261eeff1805dff67d81309
SnehaGummadi <sneha.a.gummadi@gmail.com>
2025-02-28 13:19:35 -0600

    Make bulk rna seq counts optional

--- bin/intersect_miRNA_targets_with_BulkRNAseq.py
+++ bin/intersect_miRNA_targets_with_BulkRNAseq.py
@@ -31,7 +31,7 @@ def main():
     arg_parser.add_argument(
         "--bulkRNAseq",
         type=str,
-        required=True,
+        required=False,
         help="The file with the bulk RNA-seq counts. Format should be gene_name,sample1,sample2,...,sampleN in header.",
     )
 
@@ -43,62 +43,64 @@ def main():
         help="The minimum expression of the gene to be considered.",
     )
 
-    arg_parser.add_argument(
-        "--condition",
-        type=str,
-        required=True,
-        help="The condition differential expressed miRNA.",
-    )
-
     # Check paths are correct
     args_parsed = arg_parser.parse_args()
     up_miRNA_targets_file = args_parsed.up_miRNA_targets
     down_miRNA_targets_file = args_parsed.down_miRNA_targets
     bulkRNAseq_file = args_parsed.bulkRNAseq
     min_expression = args_parsed.min_expression
-    condition = args_parsed.condition
 
+    # Read miRNA target files
     up_miRNA_targets = pd.read_csv(up_miRNA_targets_file, sep='\t', header=0)
     down_miRNA_targets = pd.read_csv(down_miRNA_targets_file, sep='\t', header=0)
-    bulkRNAseq = pd.read_csv(bulkRNAseq_file, sep=',', header=0)
+
+    print(bulkRNAseq_file)
 
     # Header of the miRNA_targets file
     # miRNAid	miRNAname	geneID	geneName	geneType	chromosome	narrowStart	narrowEnd	broadStart	broadEnd	strand	clipExpNum	degraExpNum	RBP	PITA	RNA22	miRmap	microT	miRanda	PicTar	TargetScan	TDMDScore	phyloP	pancancerNum	cellline/tissue	regulation
 
-    # Get the average gene counts across all samples
-    bulkRNAseq['mean'] = bulkRNAseq.iloc[:,1:].mean(axis=1)
+    ################################################################################################
+    # Check is if bulk RNA-seq file is provided, filter the miRNA targets by the gene expression
+    ################################################################################################
+    if bulkRNAseq_file == "NO_FILE_PROVIDED":
+        logger.info("No bulk RNA-seq file provided, only miRNA targets will be filtered.")
+    else:
+        bulkRNAseq = pd.read_csv(bulkRNAseq_file, sep=',', header=0)
+        # Get the average gene counts across all samples
+        bulkRNAseq['mean'] = bulkRNAseq.iloc[:,1:].mean(axis=1)
+        # Filter the genes with a mean expression higher than the minimum expression
+        bulkRNAseq_min = bulkRNAseq[bulkRNAseq['mean'] > min_expression]
 
-    # Filter the genes with a mean expression higher than the minimum expression
-    bulkRNAseq_min = bulkRNAseq[bulkRNAseq['mean'] > min_expression]
+        logger.info("bulkRNAseq file head with mean:")
+        logger.info(len(bulkRNAseq_min))
 
-    logger.info("bulkRNAseq file head with mean:")
-    logger.info(len(bulkRNAseq_min))
+        # Filter the up regulated miRNA targets
+        up_miRNA_targets = up_miRNA_targets[up_miRNA_targets['geneID'].isin(bulkRNAseq_min['gene_name']) | up_miRNA_targets["geneName"].isin(bulkRNAseq_min['gene_name'])]
+        up_miRNA_targets.to_csv("filtered_ENCORI_up_miRNA_DE_targets.tsv", sep='\t', index=False)
 
-    # Filter the up regulated miRNA targets
-    filtered_up_miRNA_targets = up_miRNA_targets[up_miRNA_targets['geneID'].isin(bulkRNAseq_min['gene_name']) | up_miRNA_targets["geneName"].isin(bulkRNAseq_min['gene_name'])]
-    filtered_up_miRNA_targets.to_csv(f"{condition}_filtered_ENCORI_up_miRNA_DE_targets.tsv", sep='\t', index=False)
+        # Filter the down regulated miRNA targets
+        down_miRNA_targets = down_miRNA_targets[down_miRNA_targets['geneID'].isin(bulkRNAseq_min['gene_name']) | down_miRNA_targets["geneName"].isin(bulkRNAseq_min['gene_name'])]
+        down_miRNA_targets.to_csv("filtered_ENCORI_down_miRNA_DE_targets.tsv", sep='\t', index=False)
 
-    # Filter the down regulated miRNA targets
-    filtered_down_miRNA_targets = down_miRNA_targets[down_miRNA_targets['geneID'].isin(bulkRNAseq_min['gene_name']) | down_miRNA_targets["geneName"].isin(bulkRNAseq_min['gene_name'])]
-    filtered_down_miRNA_targets.to_csv(f"{condition}_filtered_ENCORI_down_miRNA_DE_targets.tsv", sep='\t', index=False)
-
+    #############################################################################################
     # Get the number of miRNAs targeting each gene within the filtered tissue gene expression
+    #############################################################################################
     up_regulated_final_target_gene_targeted_count = (
-        filtered_up_miRNA_targets.groupby('geneName')['miRNAname']
+        up_miRNA_targets.groupby('geneName')['miRNAname']
         .nunique()
         .reset_index()
         .rename(columns={'miRNAname': 'miRNA_DE_targets_count'})
     )
 
     down_regulated_final_target_gene_targeted_count = (
-        filtered_down_miRNA_targets.groupby('geneName')['miRNAname']
+        down_miRNA_targets.groupby('geneName')['miRNAname']
         .nunique()
         .reset_index()
         .rename(columns={'miRNAname': 'miRNA_DE_targets_count'})
     )
 
-    up_regulated_final_target_gene_targeted_count.to_csv(f"{condition}_up_regulated_final_target_gene_targeted_count.tsv", sep='\t', index=False)
-    down_regulated_final_target_gene_targeted_count.to_csv(f"{condition}_down_regulated_final_target_gene_targeted_count.tsv", sep='\t', index=False)
+    up_regulated_final_target_gene_targeted_count.to_csv("up_regulated_final_target_gene_targeted_count.tsv", sep='\t', index=False)
+    down_regulated_final_target_gene_targeted_count.to_csv("down_regulated_final_target_gene_targeted_count.tsv", sep='\t', index=False)
 
 if __name__ == "__main__":
     main()
--- modules/local/intersect_miRNA_RNAseq.nf
+++ modules/local/intersect_miRNA_RNAseq.nf
@@ -1,21 +1,19 @@
 process INTERSECT_MIRNA_RNASEQ {
-    tag "$meta2.condition"
     label 'process_low'
 
     container 'docker://utdpaincenter/mirna-analysis-pandas-biopython-requests:1.3'
     
 
     input:
-    tuple val(meta2), path(up_regulated_miRNA_targets), path(down_regulated_miRNA_targets)
-    path(tissue_specific_genes)
+    tuple path(up_regulated_miRNA_targets), path(down_regulated_miRNA_targets)
+    val tissue_specific_genes
 
     output:
-    tuple val(meta2), path("*_up_regulated_final_target_gene_targeted_count.tsv"),  path("*_down_regulated_final_target_gene_targeted_count.tsv"), emit: intersect_targets
-    tuple val(meta2), path("*.log"),       emit: intersect_targets_log
+    tuple path("up_regulated_final_target_gene_targeted_count.tsv"),  path("down_regulated_final_target_gene_targeted_count.tsv"), emit: intersect_targets
+    path("*.log"),       emit: intersect_targets_log
     path "versions.yml",                   emit: versions
 
     script:
-    def condition = meta2.condition
     def args = task.ext.args ?: ''
     """
 
@@ -23,7 +21,6 @@ process INTERSECT_MIRNA_RNASEQ {
         --up_miRNA_targets ${up_regulated_miRNA_targets} \
         --down_miRNA_targets ${down_regulated_miRNA_targets} \
         --bulkRNAseq ${tissue_specific_genes} \
-        --condition ${condition} \
         ${args}
     
     cat <<-END_VERSIONS > versions.yml
